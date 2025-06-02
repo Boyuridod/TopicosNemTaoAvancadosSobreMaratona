fc9b21bbbc7c3eb3219b4ad5175d61b24e23f8a7
Yuri David Silva Duarte <yuridsd@hotmail.com>
2025-06-02 11:52:13 -0300

    Update AritmeticaModular.cpp

--- AritmeticaModular.cpp
+++ AritmeticaModular.cpp
@@ -2,6 +2,31 @@
 
 using namespace std;
 
+typedef long long longo;
+
+const long long mod = 998244353;
+
+longo somaMod(longo a, longo b) {
+    return (a + b) % mod;
+}
+
+longo multMod(longo a, longo b) {
+    return (a * b) % mod;
+}
+
+longo exponenciacaoRapida(longo base, longo expoente) {
+    if(expoente == 0) return 1;
+    if(expoente == 1) return base % mod;
+    if(expoente % 2 == 0){
+        longo aux = exponenciacaoRapida(base, expoente / 2);
+        return multMod(aux, aux);
+    }
+    else{
+        longo aux = exponenciacaoRapida(base, (expoente - 1) / 2);
+        return multMod(multMod(aux, aux), base);
+    }
+}
+
 int main(){
 
     int N = 0;
