ce9187a84912c29ea04a1b57de0a2df6a0178654
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-09-03 18:02:36 -0300

    feat: Inicio de combinatoria

---
+++ combinatoria.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int longo;
+
+map<int, longo> fats;
+
+void fatorial(longo n){
+    if(fats[n] != 0) return fats[n];
+}
+
+int main(){
+
+    
+
+    return 0;
+
+}
