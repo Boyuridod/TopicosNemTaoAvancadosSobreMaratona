640d6015e2534533acb37f302c97f0ddf436bc16
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-09-23 09:58:05 -0300

    Feat: Combinatoria e permutação

--- combinatoria.cpp
+++ combinatoria.cpp
@@ -4,15 +4,50 @@ using namespace std;
 
 typedef long long int longo;
 
-map<int, longo> fats;
+map<longo, longo> fats;
 
-void fatorial(longo n){
-    if(fats[n] != 0) return fats[n];
+longo fatorial(longo n){
+    if(n == 0){
+        fats[n] = 1;
+    }
+
+    else if(n == 1){
+        fats[n] = 1;
+    }
+
+    else if(fats[n] == 0){
+        fats[n] = n * fatorial(n-1);
+    }
+
+    return fats[n];
+}
+
+longo permutacaoSimples(longo n){
+    return fatorial(n);
+}
+
+double permutacaoComRepeticao(longo n, vector<longo> rept){
+    longo nfat = fatorial(n), mult = 1;
+    for(longo i = 0; i < rept.size(); i++){
+        mult *= fatorial(rept[i]);
+    }
+
+    return (nfat / mult);
+}
+
+void printaFatoriais(){
+    for(int i = 0; i < fats.size(); i++){
+        cout << fatorial(i) << " ";
+    }
+
+    cout << endl;
 }
 
 int main(){
-
     
+    cout << fatorial(5) << endl;
+
+    cout << permutacaoComRepeticao(10, {2, 3, 2}) << endl;
 
     return 0;
 
