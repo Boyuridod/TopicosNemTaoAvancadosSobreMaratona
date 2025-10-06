d1b79ca5b23eb9f7fef3383cceb7337cf6e4c3f1
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-10-06 11:59:47 -0300

    Fix: Molde

--- Algoritmos/1-molde.cpp
+++ Algoritmos/1-molde.cpp
@@ -7,6 +7,8 @@ using namespace std;
 typedef long long int longo;
 
 const int mod = 1e9+7; // Primo
+const int INF = 1e9; // Infinito ou (INT_MAX)
+const double EPS = 1e-9; // Epsilon (Numero muito pequeno) (ou 1e-12)
 
 int exercicio(){
 
